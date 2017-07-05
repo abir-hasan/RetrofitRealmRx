f1a7e3418f9417d31ef5e7196abf2618f65d34b3
Abir Hasan <junayed_2013>
2017-07-06 00:56:01 +0600

    Initiating Project

---
+++ app/src/main/java/me/abir/retrofitrealm/EndPointInterface.java
@@ -0,0 +1,8 @@
+package me.abir.retrofitrealm;
+
+/**
+ * Created by Abir Hasan on 02-May-17.
+ */
+
+public interface EndPointInterface {
+}
---
+++ app/src/main/java/me/abir/retrofitrealm/MainActivity.java
@@ -0,0 +1,13 @@
+package me.abir.retrofitrealm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
